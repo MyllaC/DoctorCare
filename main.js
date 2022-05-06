f5d0516c771e04df4873e9fdb804f98347802d44
Camila Cristina <criscamila09@gmail.com>
2022-05-06 12:02:16 -0300

    stage 4

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+}
+//O scroll Y = 0 é quando a barra de scroll está no começo
+
+function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
   }
 }
-//O scroll Y = 0 é quando a barra de scroll está no começo
+
+function showBackToTopButtonOnScroll() {
+  //para saber o número do Y pode usar o console.log(srollY)
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
 
 function openMenu() {
   document.body.classList.add('menu-expanded')
