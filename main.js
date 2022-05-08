c2cef4f9e7fdf9bbcebc761fb043b8b9cf9cde50
Camila Cristina <criscamila09@gmail.com>
2022-05-08 17:34:35 -0300

    js stage 5

--- main.js
+++ main.js
@@ -4,9 +4,54 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButtonOnScroll()
-}
-//O scroll Y = 0 é quando a barra de scroll está no começo
 
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  // O topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+
+  /* informações dos dados e da lógica
+  
+console.log(
+  'O topo da seção chegou ou passou da linha?,
+  sectionTopReachOrPassedTargetline
+)
+
+  */
+
+  // verificar se a base está abaixo da linha alvo
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+
+  // limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+//O scroll Y = 0 é quando a barra de scroll está no começo
 function showNavOnScroll() {
   if (scrollY > 0) {
     navigation.classList.add('scroll')
@@ -17,6 +62,7 @@ function showNavOnScroll() {
 
 function showBackToTopButtonOnScroll() {
   //para saber o número do Y pode usar o console.log(srollY)
+
   if (scrollY > 550) {
     backToTopButton.classList.add('show')
   } else {
